789e0128dff7798675c992cddac380ffdff2c868
puya91 <puya2191@gmail.com>
2024-03-12 23:02:08 +0100

    plan page added

--- src/app/nutrigenic/pages/plan/plan.component.ts
+++ src/app/nutrigenic/pages/plan/plan.component.ts
@@ -7,5 +7,25 @@ import { Subscription, debounceTime, fromEvent } from 'rxjs';
     styleUrls: ['./plan.component.scss']
 })
 export class PlanComponent {
-    constructor(){}
+    selectedFlexiDaymealButton: number = 2;
+    selectedFlexiWeekmealButton: number = 3;
+    selectedCommitDaymealButton: number = 2;
+    selectedCommitWeekmealButton: number = 3;
+    selectedCommitWeeksButton: string = '4w';
+    constructor() { }
+
+    selectButton(buttonList: string, buttonNumber: number) {
+        if (buttonList == 'flexiDay')
+            this.selectedFlexiDaymealButton = buttonNumber;
+        if (buttonList == 'flexiWeek')
+            this.selectedFlexiWeekmealButton = buttonNumber;
+        if (buttonList == 'commitDay')
+            this.selectedCommitDaymealButton = buttonNumber;
+        if (buttonList == 'commitWeek')
+            this.selectedCommitWeekmealButton = buttonNumber;
+    }
+
+    selectWeeksNumber(buttonNumber: string) {
+        this.selectedCommitWeeksButton = buttonNumber;
+    }
 }
