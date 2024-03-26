96b15c5fcc316e5373185ea4c71658e8cfa34aed
puya91 <puya2191@gmail.com>
2024-03-26 23:29:18 +0330

    push some fixes

--- src/app/nutrigenic/pages/ourshop/ourshop.component.ts
+++ src/app/nutrigenic/pages/ourshop/ourshop.component.ts
@@ -10,7 +10,7 @@ export class OurShopComponent {
     tabItems = ['Snacks', 'Health boost ingredients', 'Fine Supplements', 'Vitamins'];
     selectedMasterTab: string | null = 'Snacks';
     listCardData: any[] = [];
-    counter: number = 0;
+    counter: number = 1;
     value: number = 4
     search: string = '';
     searchIcon = true;
@@ -79,10 +79,11 @@ export class OurShopComponent {
     }
 
     increaseBtnClick() {
-        return this.counter = this.counter + 1;
+        this.counter = this.counter + 1;
     }
 
     decreaseBtnClick() {
-        return this.counter = this.counter == 0 ? 0 : this.counter - 1;
+        
+            this.counter = this.counter == 1 ? 1 : this.counter - 1;
     }
 }
